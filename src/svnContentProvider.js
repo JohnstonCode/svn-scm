6a8a3519096019873d504da198112716ffbd6eea
Chris Johnston <chris@johnstoncode.com>
2017-09-28 17:09:19 +0100

    changed code styling

--- src/svnContentProvider.js
+++ src/svnContentProvider.js
@@ -3,34 +3,35 @@ var svnSpawn = require('svn-spawn');
 var path = require('path');
 
 module.exports = {
+    client: null,
+
     init: function() {
+        this.client = this.createClient();
         vscode.workspace.registerTextDocumentContentProvider('svn', this);
     },
 
     provideTextDocumentContent: function(uri) {
         const relativePath = path.relative(vscode.workspace.rootPath, uri.fsPath).replace(/\\/g, '/');
 
-        var client = this.createClient();
-
         return new Promise((resolve, reject) => {
-            client.cmd(['ls', relativePath], function(err, data) {
+            this.client.cmd(['ls', relativePath], function(err, data) {
                 if (err) {
                     resolve('');
                     reject(err);
                 }
             });
 
-            client.cmd(['cat', '-r', 'HEAD', relativePath], function(err, data) {
+            this.client.cmd(['cat', '-r', 'HEAD', relativePath], function(err, data) {
                 resolve(data);
                 reject(err);
             });
-        });        
+        });
     },
 
-    createClient: () => {
+    createClient: function() {
         return new svnSpawn({
-          cwd: vscode.workspace.rootPath,
-          noAuthCache: true,
+            cwd: vscode.workspace.rootPath,
+            noAuthCache: true,
         });
     }
 };
