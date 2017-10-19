a7d2afb858276dff37d3b8c44e830224ab66aa18
Chris Johnston <chris@johnstoncode.com>
2017-10-19 17:32:30 +0100

    added theme icons and a resource class, not able to get scm update to work

--- src/extension.js
+++ src/extension.js
@@ -5,92 +5,6 @@ const svnSCM = require("./svnSCM");
 const svnContentProvider = require("./svnContentProvider");
 const SvnCommands = require("./commands");
 
-const checkAllFiles = svn => {
-  return new Promise((resolve, reject) => {
-    svn
-      .getStatus()
-      .then(data => resolve(data))
-      .catch(err => reject(err));
-  });
-};
-
-function createResourceUri(relativePath) {
-  const absolutePath = path.join(vscode.workspace.rootPath, relativePath);
-  return vscode.Uri.file(absolutePath);
-}
-
-const updateResourceGroup = (data, type) => {
-  var matches = [];
-
-  data.forEach(item => {
-    if (item["wc-status"].$.item == type) {
-      matches.push({ resourceUri: createResourceUri(item.$.path) });
-    }
-  });
-
-  return matches;
-};
-
-const updateChangesResourceGroup = data => {
-  const changes = [
-    "added",
-    "modified",
-    "deleted",
-    "missing",
-    "conflicted",
-    "replaced"
-  ];
-  let matches = [];
-  const iconsRootPath = path.join(__dirname, "..", "icons");
-
-  data.forEach(item => {
-    if (changes.indexOf(item["wc-status"].$.item) != -1) {
-      matches.push({
-        resourceUri: createResourceUri(item.$.path),
-        decorations: {
-          iconPath: vscode.Uri.file(
-            path.join(iconsRootPath, `${item["wc-status"].$.item}.svg`)
-          ),
-          tooltip: item["wc-status"].$.item
-        },
-        command: {
-          command: "svn.fileOpen",
-          title: "Open",
-          arguments: [createResourceUri(item.$.path)]
-        }
-      });
-    }
-  });
-
-  return matches;
-};
-
-const updateNotTrackedResourceGroup = data => {
-  let matches = [];
-  const iconsRootPath = path.join(__dirname, "icons");
-
-  data.forEach(item => {
-    if (item["wc-status"].$.item == "unversioned") {
-      matches.push({
-        resourceUri: createResourceUri(item.$.path),
-        decorations: {
-          iconPath: vscode.Uri.file(
-            path.join(iconsRootPath, `unversioned.svg`)
-          ),
-          tooltip: item["wc-status"].$.item
-        },
-        command: {
-          command: "svn.fileOpen",
-          title: "Open",
-          arguments: [createResourceUri(item.$.path)]
-        }
-      });
-    }
-  });
-
-  return matches;
-};
-
 function activate(context) {
   console.log("svn-scm is now active!");
 
@@ -103,30 +17,6 @@ function activate(context) {
   const sourceControl = new svnSCM();
   const contentProvider = new svnContentProvider();
   const svn = new Svn();
-
-  const changes = sourceControl.createResourceGroup("changes", "Changes");
-  const notTracked = sourceControl.createResourceGroup(
-    "unversioned",
-    "Not Tracked"
-  );
-
-  changes.hideWhenEmpty = true;
-  notTracked.hideWhenEmpty = true;
-
-  const main = () => {
-    return checkAllFiles(svn)
-      .then(data => {
-        changes.resourceStates = updateChangesResourceGroup(data);
-        notTracked.resourceStates = updateNotTrackedResourceGroup(data);
-      })
-      .catch(err => vscode.window.showErrorMessage(err));
-  };
-
-  watcher.onDidChange(main);
-  watcher.onDidCreate(main);
-  watcher.onDidDelete(main);
-
-  main();
 
   context.subscriptions.push(disposable);
 }
--- src/svnSCM.js
+++ src/svnSCM.js
@@ -1,6 +1,10 @@
-var { Uri, scm } = require("vscode");
+var { Uri, scm, workspace } = require("vscode");
+const Svn = require("./svn");
+const Resource = require("./Resource");
 
 function svnSCM() {
+  this.svn = new Svn();
+  this.watcher = workspace.createFileSystemWatcher("**");
   this.sourceControl = scm.createSourceControl("svn", "svn");
   this.sourceControl.acceptInputCommand = {
     command: "svn.commitWithMessage",
@@ -8,7 +12,20 @@ function svnSCM() {
   };
   this.sourceControl.quickDiffProvider = this;
 
-  return this.sourceControl;
+  this.changes = this.sourceControl.createResourceGroup("changes", "Changes");
+  this.notTracked = this.sourceControl.createResourceGroup(
+    "unversioned",
+    "Not Tracked"
+  );
+
+  this.changes.hideWhenEmpty = true;
+  this.notTracked.hideWhenEmpty = true;
+
+  this.watcher.onDidChange(this.update);
+  this.watcher.onDidCreate(this.update);
+  this.watcher.onDidDelete(this.update);
+
+  this.update();
 }
 
 svnSCM.prototype.provideOriginalResource = uri => {
@@ -17,6 +34,45 @@ svnSCM.prototype.provideOriginalResource = uri => {
   }
 
   return new Uri().with({ scheme: "svn", query: uri.path, path: uri.path });
+};
+
+svnSCM.prototype.update = function() {
+  let changes = [];
+  let notTracked = [];
+
+  this.changes.resourceStates = [];
+  this.notTracked.resourceStates = [];
+
+  this.svn
+    .getStatus()
+    .then(result => {
+      let changes = [];
+      let notTracked = [];
+
+      result.forEach(item => {
+        switch (item["wc-status"].$.item) {
+          case "modified":
+          case "deleted":
+          case "conflicted":
+          case "replaced":
+          case "missing":
+          case "added":
+            changes.push(new Resource(item.$.path, item["wc-status"].$.item));
+            break;
+          case "unversioned":
+            notTracked.push(
+              new Resource(item.$.path, item["wc-status"].$.item)
+            );
+            break;
+        }
+      });
+
+      this.changes.resourceStates = changes;
+      this.notTracked.resourceStates = notTracked;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
 
 module.exports = svnSCM;
