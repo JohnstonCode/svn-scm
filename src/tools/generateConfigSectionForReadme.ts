39124ef7eb8770714cc1a8984fa1f93da380b25f
Kenny John Jacob <johnjacobkenny@gmail.com>
2018-03-13 23:26:10 +0530

    Add script to generate config section for readme

---
+++ src/tools/generateConfigSectionForReadme.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import { cwd } from "process";
+
+const packageJsonPath: string = join(cwd(), "package.json");
+const packageJson = JSON.parse(readFileSync(packageJsonPath));
+const properties = packageJson.contributes.configuration.properties;
+
+console.log("|Config|Description|Default|");
+console.log("|-|-|-|");
+
+Object.keys(properties).forEach( val => {
+    let prop = properties[val];
+    console.log(`|\`${val}\`|${prop.description}|\`${prop.default}\`|);
+});
