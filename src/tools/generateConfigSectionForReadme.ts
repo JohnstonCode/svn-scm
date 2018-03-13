ffe8282a7a12be5a6fe7afe44792fb3dfb69c2a3
Edgard Lorraine Messias <edgardmessias@gmail.com>
2018-03-13 15:51:30 -0300

    Fixed return of readFileSync

--- src/tools/generateConfigSectionForReadme.ts
+++ src/tools/generateConfigSectionForReadme.ts
@@ -3,7 +3,7 @@ import { join } from "path";
 import { cwd } from "process";
 
 const packageJsonPath: string = join(cwd(), "package.json");
-const packageJson = JSON.parse(readFileSync(packageJsonPath));
+const packageJson = JSON.parse(readFileSync(packageJsonPath, {encoding: 'utf8'}));
 const properties = packageJson.contributes.configuration.properties;
 
 console.log("|Config|Description|Default|");
