28f3801fe561b24ea3f01b7db65e85e5e5915f1d
chris johnston <chris@johnstoncode.com>
2018-08-22 18:03:12 +0100

    feat:Added left click commands to incming change nodes, fixes #324

--- src/treeView/nodes/incomingChangeNode.ts
+++ src/treeView/nodes/incomingChangeNode.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { TreeItem, TreeItemCollapsibleState, Uri } from "vscode";
 import { Repository } from "../../repository";
+import { Resource } from "../../resource";
 import { getIconUri } from "../../uri";
 import BaseNode from "./baseNode";
 
@@ -31,11 +32,37 @@ export default class IncomingChangeNode implements BaseNode {
       light: getIconUri(`status-${this.type}`, "light")
     };
     item.contextValue = this.contextValue;
+    item.command = this.getCommand();
 
     return item;
   }
 
   public getChildren(): Promise<BaseNode[]> {
     return Promise.resolve([]);
+  }
+
+  public getCommand() {
+    switch (this.type) {
+      case "modified":
+        return {
+          command: "svn.openChangeHead",
+          title: "Open Changes with HEAD",
+          arguments: [this.uri]
+        };
+      case "deleted":
+        return {
+          command: "svn.openFile",
+          title: "Open File",
+          arguments: [this.uri]
+        };
+      case "added":
+        return {
+          command: "svn.openHEADFile",
+          title: "Open File (HEAD)",
+          arguments: [
+            new Resource(this.uri, this.type, undefined, "none", true)
+          ]
+        };
+    }
   }
 }
