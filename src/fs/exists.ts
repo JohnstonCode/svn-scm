d82f973c6c95be47eddb382887a38bd4f39a49b1
chris johnston <chris@johnstoncode.com>
2019-03-29 12:02:37 +0000

    testing promisify

--- src/fs/exists.ts
+++ src/fs/exists.ts
@@ -1,7 +1,11 @@
-import { access } from "original-fs";
+import { access, exists as fsExists } from "original-fs";
+import * as util from "util";
 
-export function exists(path: string): Promise<boolean> {
-  return new Promise((resolve, reject) => {
-    access(path, err => (err ? resolve(false) : resolve(true)));
-  });
-}
+
+export const exists = util.promisify(fsExists);
+
+// export function exists(path: string): Promise<boolean> {
+//   return new Promise((resolve, reject) => {
+//     access(path, err => (err ? resolve(false) : resolve(true)));
+//   });
+// }
