94a46a5072418bac7a10c4aba0102d5f4280c58d
chris johnston <chris@johnstoncode.com>
2019-03-29 10:51:42 +0000

    exsists now only returns bool and never rejects

--- src/fs/exists.ts
+++ src/fs/exists.ts
@@ -2,6 +2,6 @@ import { access } from "original-fs";
 
 export function exists(path: string): Promise<boolean> {
   return new Promise((resolve, reject) => {
-    access(path, err => (err ? reject(err) : resolve(true)));
+    access(path, err => (err ? resolve(false) : resolve(true)));
   });
 }
