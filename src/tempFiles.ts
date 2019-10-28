02b50079d572963b31fc72e876f98c9ebe68ac6a
Christopher <me@chrisjohnston.co.uk>
2019-10-28 11:16:15 +0000

    Fixed typo

--- src/tempFiles.ts
+++ src/tempFiles.ts
@@ -7,16 +7,16 @@ import * as crypto from "crypto";
 export const tempdir = path.join(os.tmpdir(), "vscode-svn");
 
 export async function dumpSvnFile(
-  snvUri: Uri,
+  svnUri: Uri,
   revision: string,
   payload: string
 ): Promise<Uri> {
   if (!(await exists(tempdir))) {
     await mkdir(tempdir);
   }
-  const fname = `r${revision}_${path.basename(snvUri.fsPath)}`;
+  const fname = `r${revision}_${path.basename(svnUri.fsPath)}`;
   const hash = crypto.createHash("md5");
-  const data = hash.update(snvUri.fsPath);
+  const data = hash.update(svnUri.fsPath);
   const filePathHash = data.digest("hex");
   const fpath = path.join(tempdir, filePathHash, fname);
   await writeFile(fpath, payload);
