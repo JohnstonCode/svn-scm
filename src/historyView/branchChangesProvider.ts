788c44e9c51384495c425df8b4660daa4044f12a
Marc Reisner <reisner.marc@gmail.com>
2019-11-10 15:12:20 -0600

    Revert "Fix loop"
    This reverts commit f4a498a2b88ba3ddfff013aa0486f10ffd9e4d2e.

--- src/historyView/branchChangesProvider.ts
+++ src/historyView/branchChangesProvider.ts
@@ -6,7 +6,7 @@ import {
   EventEmitter
 } from "vscode";
 import { Model } from "../model";
-import { ISvnPathChange, Status } from "../common/types";
+import { ISvnPathChange, Status, IModelChangeEvent } from "../common/types";
 import { openDiff, getIconObject, openFileRemote } from "./common";
 import { dispose } from "../util";
 
@@ -26,14 +26,12 @@ export class BranchChangesProvider
     );
 
     this._dispose.push(
-      commands.registerCommand(
-        "svn.branchchanges.refresh",
-        () => this._onDidChangeTreeData.fire(),
-        this
-      )
+      commands.registerCommand("svn.branchchanges.refresh", this.refresh, this)
     );
 
-    this.model.onDidChangeRepository(() => this._onDidChangeTreeData.fire());
+    this.model.onDidChangeRepository(async (_e: IModelChangeEvent) => {
+      return this.refresh();
+    });
   }
 
   dispose() {
@@ -65,6 +63,13 @@ export class BranchChangesProvider
   }
 
   getChildren(element?: ISvnPathChange): Promise<ISvnPathChange[]> {
+    return this.refresh(element, false);
+  }
+
+  public async refresh(
+    element?: ISvnPathChange,
+    fire: boolean = true
+  ): Promise<ISvnPathChange[]> {
     if (element !== undefined) {
       return Promise.resolve([]);
     }
@@ -75,9 +80,14 @@ export class BranchChangesProvider
       changes.push(repo.getChanges());
     }
 
-    return Promise.all(changes).then(value =>
+    const result = await Promise.all(changes).then(value =>
       value.reduce((prev, curr) => prev.concat(curr), [])
     );
+
+    if (fire) {
+      this._onDidChangeTreeData.fire();
+    }
+    return result;
   }
 
   public async openDiffCmd(element: ISvnPathChange) {
